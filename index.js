44c2df6975c4070856226231d14e16d907d8d0b3
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-05-07 19:19:59 +0200

    Task completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/*
 const nastartujMinutku = (e) => {
   e.preventDefault();
   let time = Number(document.querySelector('.time-input').value);
@@ -17,6 +17,52 @@ const nastartujMinutku = (e) => {
       sekundyElm.textContent = String(time);
     }
     if (time <= 0) {
+      clearInterval(casovac);
+      alarmElm.classList.add('alarm--ring');
+      document.querySelector('audio').play();
+    }
+  };
+
+  const casovac = setInterval(odpocet, 1000);
+};
+
+const formularElm = document.querySelector('.controls');
+formularElm.addEventListener('submit', nastartujMinutku);
+
+*/
+
+/*
+Vylepšete program naší kuchyňské minutky tak, aby zobrazovala nejen vteřiny, ale i minuty. Layout stránky je na to připraven. Nechejte uživatele zadat minuty, nikoliv vteřiny, a správně odpočítávejte čas až k nule.
+
+Dejte pozor na správné formátování, kdy jednociferné hodnoty je zvykem zobrazovat s nulou na začátku. Vzpomeňte si, že na řetězcích máme metodu padStart, která nám s tímto může velmi pomoct.
+*/
+const nastartujMinutku = (e) => {
+  e.preventDefault();
+  let minutes = Number(document.querySelector('.time-input').value);
+  let seconds = 0;
+
+  const minutyElm = document.querySelector('.alarm__minutes');
+  const sekundyElm = document.querySelector('.alarm__seconds');
+  const alarmElm = document.querySelector('.alarm');
+
+  // pro opakované použití
+  alarmElm.classList.remove('alarm--ring');
+
+  minutyElm.textContent = String(minutes) + ':';
+  sekundyElm.textContent = String(seconds).padStart(2, '0');
+
+  const odpocet = () => {
+    if (minutes >= 0) {
+      if (seconds > 0) {
+        seconds -= 1;
+      } else {
+        minutes -= 1;
+        seconds = 59;
+      }
+      minutyElm.textContent = String(minutes) + ':';
+      sekundyElm.textContent = String(seconds).padStart(2, '0');
+    }
+    if (minutes <= 0 && seconds <= 0) {
       clearInterval(casovac);
       alarmElm.classList.add('alarm--ring');
       document.querySelector('audio').play();
